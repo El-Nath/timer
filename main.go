26f7a3814c9b77ca69822ef0d62e288516013047
gamakireta <ariel@codigo.id>
2020-03-24 14:27:17 +0700

    main.go : validate user's input
    Validating our input in order to direct the user to the right process
    
    Signed-off-by: gamakireta <gamakireta@gmail>

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	arr := []int{1, 2, 4, 2, 3, 5, 2, 3, 1, 3}
@@ -11,5 +14,24 @@ func main() {
 	_, err := fmt.Scan(&input)
 	if err != nil {
 		fmt.Println(err.Error())
+	}
+
+	switch input {
+	case "s":
+		//singlethread
+	case "m":
+		//multithread
+		fmt.Print("Input number of concurrency: ")
+		_, err := fmt.Scan(&cN)
+		n, err := strconv.Atoi(cN)
+		if err != nil {
+			fmt.Println("--Not a valid option, terminating now--")
+			break
+		}
+
+	default:
+		//faulty input
+		fmt.Println("--Not a valid option, terminating now--")
+		break
 	}
 }
