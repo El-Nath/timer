e98fa4b12cddfb43a0bfc5c45ebca2703eefd6eb
gamakireta <ariel@codigo.id>
2020-03-24 14:24:19 +0700

    main.go : setting up input
    Setting up input we're gonna get from user, whether it's a single thread
    or multithread
    
    Signed-off-by: gamakireta <gamakireta@gmail.com>

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
 	arr := []int{1, 2, 4, 2, 3, 5, 2, 3, 1, 3}
+	type Inputs []string
+	var input, cN string
+
+	fmt.Print("Sleep single threaded or multi threaded (s/m): ")
+	_, err := fmt.Scan(&input)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 }
